9dc8a8ef077fcf4f729e425b3dc8cb3325cd3cc1
Andrei Punko <Andrei_Punko@epam.com>
2018-11-21 17:51:52 +0300

    Add couple of classes with javadoc annotations

---
+++ src/main/java/by/andd3dfx/Person.java
@@ -0,0 +1,25 @@
+package by.andd3dfx;
+
+public class Person {
+    /**
+     * This is a first name
+     */
+    private String firstName;
+    private String lastName;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+}
