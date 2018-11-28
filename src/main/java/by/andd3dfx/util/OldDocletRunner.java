cec218c3452ca08ffb07dcc293b907edc8c34584
Andrei Punko <Andrei_Punko@epam.com>
2018-11-28 16:50:31 +0300

    Extract two methods for options reading into separate util class

--- src/main/java/by/andd3dfx/util/OldDocletRunner.java
+++ src/main/java/by/andd3dfx/util/OldDocletRunner.java
@@ -1,12 +1,5 @@
 package by.andd3dfx.util;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.StringTokenizer;
-
 /**
  * To use runner just pass as commandline param path+name of file with doclet name and its params
  *
@@ -28,43 +21,6 @@ public class OldDocletRunner {
             return;
         }
 
-        com.sun.tools.javadoc.Main.execute("javadoc", processOptionsFile(args[0]));
-    }
-
-    private static String[] processOptionsFile(final String filename) {
-        List<String> jargs = new ArrayList<>();
-
-        String options = readOptionsFromFile(filename);
-        StringTokenizer tokens = new StringTokenizer(options);
-        while (tokens.hasMoreTokens()) {
-            jargs.add(tokens.nextToken());
-        }
-
-        return jargs.toArray(new String[0]);
-    }
-
-    private static String readOptionsFromFile(String filename) {
-        StringBuffer buffer = new StringBuffer();
-        BufferedReader bufferedReader = null;
-        try {
-            bufferedReader = new BufferedReader(new FileReader(filename));
-            String line;
-            while ((line = bufferedReader.readLine()) != null) {
-                buffer.append(line).append("\n");
-            }
-        } catch (final IOException ioe) {
-            ioe.printStackTrace();
-            buffer.setLength(0);
-        } finally {
-            if (bufferedReader != null) {
-                try {
-                    bufferedReader.close();
-                } catch (IOException ioe) {
-                    ioe.printStackTrace();
-                }
-            }
-        }
-
-        return buffer.toString();
+        com.sun.tools.javadoc.Main.execute("javadoc", OptionsFileUtil.processOptionsFile(args[0]));
     }
 }
