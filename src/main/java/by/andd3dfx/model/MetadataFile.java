e873a2ae7b6a1bd8fd881b53e88aca888f481f97
Andrei Punko <Andrei_Punko@epam.com>
2018-12-03 14:45:54 +0300

    Give appropriate names to some classes

---
+++ src/main/java/by/andd3dfx/model/MetadataFile.java
@@ -0,0 +1,26 @@
+package by.andd3dfx.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MetadataFile {
+
+    private List<MetadataFileItem> items = new ArrayList<>();
+    private List<MetadataFileItem> references = new ArrayList<>();
+
+    public List<MetadataFileItem> getItems() {
+        return items;
+    }
+
+    public void setItems(List<MetadataFileItem> items) {
+        this.items = items;
+    }
+
+    public List<MetadataFileItem> getReferences() {
+        return references;
+    }
+
+    public void setReferences(List<MetadataFileItem> references) {
+        this.references = references;
+    }
+}
