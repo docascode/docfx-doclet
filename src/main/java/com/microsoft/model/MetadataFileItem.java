850f796ecb0beb81d0df70bcffa6182143e0763d
Andrei Punko <Andrei_Punko@epam.com>
2018-12-17 17:47:23 +0300

    Cleanup for MetadataFileItem: - Remove extra fields - Reorder fields according to their order in output files

--- src/main/java/com/microsoft/model/MetadataFileItem.java
+++ src/main/java/com/microsoft/model/MetadataFileItem.java
@@ -1,39 +1,30 @@
 package com.microsoft.model;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 import org.apache.commons.lang3.StringUtils;
 
 public class MetadataFileItem {
 
     private String uid;
-    private List<String> children = new ArrayList<>();
-    private String parent;
-
     private String id;
-    private String alias;
+    private String parent;
+    private List<String> children = new ArrayList<>();
+    private String href;
     private String name;
+    private String nameWithType;
     private String fullName;
     private String overload;
     private String type;
-    private String url;
-
-    private Map<String, String> extraProperties = new HashMap<>();
-
-    // Not present in spec
-    private String href;
-    private String nameWithType;
     private String packageName;
     private String summary;
     private String content;
-    private String superclass;
-    private List<TypeParameter> exceptions = new ArrayList<>();
     private List<TypeParameter> typeParameters = new ArrayList<>();
     private List<TypeParameter> parameters = new ArrayList<>();
     private String returnType;
     private String returnDescription;
+    private String superclass;
+    private List<TypeParameter> exceptions = new ArrayList<>();
 
     public String getUid() {
         return uid;
@@ -41,22 +32,6 @@ public class MetadataFileItem {
 
     public void setUid(String uid) {
         this.uid = uid;
-    }
-
-    public List<String> getChildren() {
-        return children;
-    }
-
-    public void setChildren(List<String> children) {
-        this.children = children;
-    }
-
-    public String getParent() {
-        return parent;
-    }
-
-    public void setParent(String parent) {
-        this.parent = parent;
     }
 
     public String getId() {
@@ -67,12 +42,28 @@ public class MetadataFileItem {
         this.id = id;
     }
 
-    public String getAlias() {
-        return alias;
+    public String getParent() {
+        return parent;
     }
 
-    public void setAlias(String alias) {
-        this.alias = alias;
+    public void setParent(String parent) {
+        this.parent = parent;
+    }
+
+    public List<String> getChildren() {
+        return children;
+    }
+
+    public void setChildren(List<String> children) {
+        this.children = children;
+    }
+
+    public String getHref() {
+        return href;
+    }
+
+    public void setHref(String href) {
+        this.href = href;
     }
 
     public String getName() {
@@ -81,6 +72,14 @@ public class MetadataFileItem {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public String getNameWithType() {
+        return nameWithType;
+    }
+
+    public void setNameWithType(String nameWithType) {
+        this.nameWithType = nameWithType;
     }
 
     public String getFullName() {
@@ -107,38 +106,6 @@ public class MetadataFileItem {
         this.type = type;
     }
 
-    public String getUrl() {
-        return url;
-    }
-
-    public void setUrl(String url) {
-        this.url = url;
-    }
-
-    public Map<String, String> getExtraProperties() {
-        return extraProperties;
-    }
-
-    public void setExtraProperties(Map<String, String> extraProperties) {
-        this.extraProperties = extraProperties;
-    }
-
-    public String getHref() {
-        return href;
-    }
-
-    public void setHref(String href) {
-        this.href = href;
-    }
-
-    public String getNameWithType() {
-        return nameWithType;
-    }
-
-    public void setNameWithType(String nameWithType) {
-        this.nameWithType = nameWithType;
-    }
-
     public String getPackageName() {
         return packageName;
     }
@@ -161,22 +128,6 @@ public class MetadataFileItem {
 
     public void setContent(String content) {
         this.content = content;
-    }
-
-    public String getSuperclass() {
-        return superclass;
-    }
-
-    public void setSuperclass(String superclass) {
-        this.superclass = superclass;
-    }
-
-    public List<TypeParameter> getExceptions() {
-        return exceptions;
-    }
-
-    public void setExceptions(List<TypeParameter> exceptions) {
-        this.exceptions = exceptions;
     }
 
     public List<TypeParameter> getTypeParameters() {
@@ -209,6 +160,22 @@ public class MetadataFileItem {
 
     public void setReturnDescription(String returnDescription) {
         this.returnDescription = returnDescription;
+    }
+
+    public String getSuperclass() {
+        return superclass;
+    }
+
+    public void setSuperclass(String superclass) {
+        this.superclass = superclass;
+    }
+
+    public List<TypeParameter> getExceptions() {
+        return exceptions;
+    }
+
+    public void setExceptions(List<TypeParameter> exceptions) {
+        this.exceptions = exceptions;
     }
 
     public String toItemString() {
@@ -288,8 +255,8 @@ public class MetadataFileItem {
             + "  fullName: " + fullName + "\n"
             + "  type: " + type + "\n"
             + "  summary: " + summary + "\n"
-            + "  syntax:\n";
-        result += "    content: " + content + "\n";
+            + "  syntax:\n"
+            + "    content: " + content + "\n";
 
         if (typeParameters.isEmpty()) {
             return result;
