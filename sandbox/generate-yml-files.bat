3e9a3ab8a2fca18f1137c01e68d03683bc1b1859
Andrei Punko <Andrei_Punko@epam.com>
2019-02-05 13:02:49 +0300

    Put libs into separate folder

--- sandbox/generate-yml-files.bat
+++ sandbox/generate-yml-files.bat
@@ -1,6 +1,6 @@
 
-set SOURCES_FOLDER="../target/unpacked-sources"
-set GENERATED_FOLDER="../target/generated-yml-files"
+set SOURCES_FOLDER="./unpacked-sources"
+set GENERATED_FOLDER="./generated-yml-files"
 
 echo "Remove folders"
 rmdir /S /Q %SOURCES_FOLDER%
@@ -11,7 +11,7 @@ mkdir %SOURCES_FOLDER%
 
 echo "Extract sources from jar file"
 pushd %SOURCES_FOLDER%
-jar xf ../../sandbox/junit-4.12-sources.jar
+jar xf ../libs/junit-4.12-sources.jar
 popd
 
 echo "Generate yml files"
@@ -19,7 +19,7 @@ javadoc ^
 -encoding UTF-8 ^
 -docletpath ../target/docfx-doclet-1.0-SNAPSHOT-jar-with-dependencies.jar ^
 -doclet com.microsoft.doclet.DocFxDoclet ^
--cp ./hamcrest-core-1.3.jar ^
+-cp ./libs/hamcrest-core-1.3.jar ^
 -sourcepath %SOURCES_FOLDER% ^
 -outputpath %GENERATED_FOLDER% ^
 -subpackages org:junit
