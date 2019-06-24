a71e7b7b85c9c981a948e3c1f98c47687c14f749
anmeng10101 <33647870+anmeng10101@users.noreply.github.com>
2019-06-24 18:01:40 +0800

    resolve "name" of "spec.java" from fullname to shortname (#16)

--- src/main/java/com/microsoft/model/SpecViewModel.java
+++ src/main/java/com/microsoft/model/SpecViewModel.java
@@ -1,38 +1,52 @@
 package com.microsoft.model;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+import java.util.Collections;
+
+import org.apache.commons.lang3.StringUtils;
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 
 @JsonPropertyOrder({"uid", "name", "fullName", "isExternal"})
+
 public class SpecViewModel {
 
-    private String Uid;
-
-    private String Name;
-
-    private String FullName;
-
+    private String uid;
+    private String name;
+    private String fullName;
     private boolean isExternal;
 
-    public SpecViewModel(String uid, String name, String fullName) {
-        this.Uid = uid;
-        this.Name = name;
-        this.FullName = fullName;
-    }
-
-    public SpecViewModel(String name, String fullName) {
-        this.Name = name;
-        this.FullName = fullName;
+    public SpecViewModel(String uid, String fullName) {
+        this.uid = uid;
+        this.name = getShortName(fullName);
+        this.fullName = fullName;
     }
 
     public String getUid() {
-        return Uid;
+        return uid;
     }
 
     public String getName() {
-        return Name;
+        return name;
     }
 
     public String getFullName() {
-        return FullName;
+        return fullName;
+    }
+
+    String getShortName(String fullName) {
+
+        StringBuilder singleValue = new StringBuilder();
+        Optional.ofNullable(fullName).ifPresent(
+                Param -> {
+                    List<String> strList = new ArrayList<>();
+                    strList = Arrays.asList(StringUtils.split(Param, "."));
+                    Collections.reverse(strList);
+                    singleValue.append(strList.get(0));
+                }
+        );
+        return singleValue.toString();
     }
 }
